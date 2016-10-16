bdb8fae138738b8bc56acdeae956183dbe2b390f
Takayuki Matsumura <matsumura0219@gmail.com>
2016-10-17 01:19:11 +0900

    use FlipFlop class instead of Base class

--- lib/logiccircuit/flipflop.rb
+++ lib/logiccircuit/flipflop.rb
@@ -1,38 +1,36 @@
 #!/usr/bin/env ruby
 #-*- coding: utf-8 -*-
 module LogicCircuit
-  module FlipFlop
-    class Base < Element
+  class FlipFlop < Element
+  end
+  
+  class RS < FlipFlop
+    def initialize r, s
+      @snand = Nand.new Not.new(s),
+                        Nand.new(Not.new(r), self)
+      super
     end
     
-    class RS < Base
-      def initialize r, s
-        @snand = Nand.new Not.new(s),
-                          Nand.new(Not.new(r), self)
-        super
-      end
-      
-      def drive
-        @snand.drive
-      end
+    def drive
+      @snand.drive
+    end
+  end
+  
+  class JK < FlipFlop
+    def initialize j, k, clock
+      @jnand = Nand.new Nand.new(j, clock, Not.new(self)),
+                        Nand.new(self, Nand.new(k, clock, self))
+      super
     end
     
-    class JK < Base
-      def initialize j, k, clock
-        @jnand = Nand.new Nand.new(j, clock, Not.new(self)),
-                          Nand.new(self, Nand.new(k, clock, self))
-        super
-      end
-      
-      def drive
-        @jnand.drive
-      end
+    def drive
+      @jnand.drive
     end
-
-    class T < JK
-      def initialize clock
-        super Wire.new(1), Wire.new(1), clock
-      end
+  end
+  
+  class T < JK
+    def initialize clock
+      super Wire.new(1), Wire.new(1), clock
     end
   end
 end
--- spec/flipflop_spec.rb
+++ spec/flipflop_spec.rb
@@ -2,94 +2,92 @@ require "spec_helper"
 using LogicCircuit::Boolean
 
 module LogicCircuit
-  module FlipFlop
-    describe RS do
-      it "outputs 1 when s = 1 and r = 0" do
-        r = Wire.new
-        s = Wire.new 1
-        f = RS.new r, s
-        expect(f.output.to_i).to eq(1)
-      end
-      it "outputs 1 when s = 1 and r = 0 and then s = 0" do
-        r = Wire.new
-        s = Wire.new 1
-        f = RS.new r, s
-        s = Wire.new 0
-        expect(f.output.to_i).to eq(1)
-      end
-      it "outputs 0 when s = 0 and r = 1" do
-        r = Wire.new 1
-        s = Wire.new
-        f = RS.new r, s
-        expect(f.output.to_i).to eq(0)
-      end
-      it "outputs 0 when s = 0 and r = 1 and then r = 0" do
-        r = Wire.new 1
-        s = Wire.new
-        f = RS.new r, s
-        r = 0
-        expect(f.output.to_i).to eq(0)
-      end
+  describe RS do
+    it "outputs 1 when s = 1 and r = 0" do
+      r = Wire.new
+      s = Wire.new 1
+      f = RS.new r, s
+      expect(f.output.to_i).to eq(1)
     end
-
-    describe JK do
-      it "outputs 1 when j = 1 and k = 0" do
-        clock = Clock.new
-        j = Wire.new 1
-        k = Wire.new
-        f = JK.new j, k, clock
-        clock.tick
-        expect(f.output.to_i).to eq(1)
-      end
-      it "outputs 0 when j = 0 and k = 1" do
-        clock = Clock.new
-        j = Wire.new
-        k = Wire.new 1
-        f = JK.new j, k, clock
-        clock.tick
-        expect(f.output.to_i).to eq(0)
-      end
-      it "outputs 0 when j = 1 and k = 0 and then j = k = 1" do
-        clock = Clock.new
-        j = Wire.new 1
-        k = Wire.new
-        f = JK.new j, k, clock
-        clock.tick
-        expect(f.output.to_i).to eq(1)
-        clock.tack
-        k.input = 1
-        clock.tick
-        expect(f.output.to_i).to eq(0)
-      end
-      it "outputs 1 when j = 0 and k = 1 and then j = k = 1" do
-        clock = Clock.new
-        j = Wire.new
-        k = Wire.new 1
-        f = JK.new j, k, clock
-        clock.tick
-        expect(f.output.to_i).to eq(0)
-        clock.tack
-        j.input = 1
-        clock.tick
-        expect(f.output.to_i).to eq(1)
-      end
+    it "outputs 1 when s = 1 and r = 0 and then s = 0" do
+      r = Wire.new
+      s = Wire.new 1
+      f = RS.new r, s
+      s = Wire.new 0
+      expect(f.output.to_i).to eq(1)
     end
-    
-    describe T do
-      it "outputs 1 when output was 0 and then clock = 1" do
-        clock = Clock.new
-        t = T.new clock
-        clock.tick
-        expect(t.output.to_i).to eq(1)
-      end
-      it "outputs 0 when output was 1 and then clock = 1" do
-        clock = Clock.new
-        t = T.new clock
-        clock.tick
-        clock.tack
-        clock.tick
-        expect(t.output.to_i).to eq(0)
-      end
+    it "outputs 0 when s = 0 and r = 1" do
+      r = Wire.new 1
+      s = Wire.new
+      f = RS.new r, s
+      expect(f.output.to_i).to eq(0)
+    end
+    it "outputs 0 when s = 0 and r = 1 and then r = 0" do
+      r = Wire.new 1
+      s = Wire.new
+      f = RS.new r, s
+      r = 0
+      expect(f.output.to_i).to eq(0)
+    end
+  end
+  
+  describe JK do
+    it "outputs 1 when j = 1 and k = 0" do
+      clock = Clock.new
+      j = Wire.new 1
+      k = Wire.new
+      f = JK.new j, k, clock
+      clock.tick
+      expect(f.output.to_i).to eq(1)
+    end
+    it "outputs 0 when j = 0 and k = 1" do
+      clock = Clock.new
+      j = Wire.new
+      k = Wire.new 1
+      f = JK.new j, k, clock
+      clock.tick
+      expect(f.output.to_i).to eq(0)
+    end
+    it "outputs 0 when j = 1 and k = 0 and then j = k = 1" do
+      clock = Clock.new
+      j = Wire.new 1
+      k = Wire.new
+      f = JK.new j, k, clock
+      clock.tick
+      expect(f.output.to_i).to eq(1)
+      clock.tack
+      k.input = 1
+      clock.tick
+      expect(f.output.to_i).to eq(0)
+    end
+    it "outputs 1 when j = 0 and k = 1 and then j = k = 1" do
+      clock = Clock.new
+      j = Wire.new
+      k = Wire.new 1
+      f = JK.new j, k, clock
+      clock.tick
+      expect(f.output.to_i).to eq(0)
+      clock.tack
+      j.input = 1
+      clock.tick
+      expect(f.output.to_i).to eq(1)
+    end
+  end
+  
+  describe T do
+    it "outputs 1 when output was 0 and then clock = 1" do
+      clock = Clock.new
+      t = T.new clock
+      clock.tick
+      expect(t.output.to_i).to eq(1)
+    end
+    it "outputs 0 when output was 1 and then clock = 1" do
+      clock = Clock.new
+      t = T.new clock
+      clock.tick
+      clock.tack
+      clock.tick
+      expect(t.output.to_i).to eq(0)
     end
   end
 end  
