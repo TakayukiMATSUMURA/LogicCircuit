5b9110497b55a8198400ae0f3c52b0c55b2258ed
Takayuki Matsumura <matsumura0219@gmail.com>
2016-10-16 16:54:05 +0900

    add to_b test

--- spec/boolean_spec.rb
+++ spec/boolean_spec.rb
@@ -9,12 +9,25 @@ module LogicCircuit
     it "refines TrueClass(true.to_s == '1')" do
       expect(true.to_s).to eq('1')
     end
+    it "refines TrueClass(true.to_b == true)" do
+      expect(true.to_b).to eq(true)
+    end
     
     it "refines FalseClass(false.to_i == 0)" do
       expect(false.to_i).to eq(0)
     end
     it "refines FalseClass(false.to_s == '0')" do
       expect(false.to_s).to eq('0')
+    end
+    it "refines FalseClass(false.to_b == false)" do
+      expect(false.to_b).to eq(false)
+    end
+
+    it "refines Integer(0.to_b == false)" do
+      expect(0.to_b).to eq(false)
+    end
+    it "refines Integer(1.to_b == true)" do
+      expect(1.to_b).to eq(true)
     end
   end
 end
