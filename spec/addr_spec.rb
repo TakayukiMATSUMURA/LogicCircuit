c2ca0f28c498c08044a518c348c473e925cfd55d
Takayuki Matsumura <matsumura0219@gmail.com>
2016-10-17 23:46:24 +0900

    add s

--- spec/addr_spec.rb
+++ spec/addr_spec.rb
@@ -4,14 +4,14 @@ using LogicCircuit::Boolean
 
 module LogicCircuit
   describe HalfAddr do
-    it "output s = 0, c = 0 when both inputs are 0" do
+    it "outputs s = 0, c = 0 when both inputs are 0" do
       a = Wire.new
       b = Wire.new
       h = HalfAddr.new a, b
       expect(h.s.output.to_i).to eq(0)
       expect(h.c.output.to_i).to eq(0)
     end
-    it "output s = 1, c = 0 when one input is 1" do
+    it "outputs s = 1, c = 0 when one input is 1" do
       a = Wire.new 1
       b = Wire.new
       h = HalfAddr.new a, b
@@ -22,7 +22,7 @@ module LogicCircuit
       expect(h.s.output.to_i).to eq(1)
       expect(h.c.output.to_i).to eq(0)
     end
-    it "output s = 0, c = 1 when both inputs are 1" do
+    it "outputs s = 0, c = 1 when both inputs are 1" do
       a = Wire.new 1
       b = Wire.new 1
       h = HalfAddr.new a, b
